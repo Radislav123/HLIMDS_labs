064513c613ba0161b34d2eeaa84bf671024fce17
meowmik <tosegida@edu.hse.ru>
2021-11-17 14:12:22 +0300

    opencv_show_image.py was fixed #3

--- lab_1/opencv_show_image.py
+++ lab_1/opencv_show_image.py
@@ -3,6 +3,6 @@ import cv2
 
 if __name__ == "__main__":
     image = cv2.imread("attachments/bread_board.PNG")
-    cv2.imshow('', image)
+    cv2.imshow('my image', image)
     cv2.waitKey(5000)
     cv2.destroyAllWindows()
