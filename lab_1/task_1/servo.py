eb0a441bef2fade4e52fce5eb42b6210705ee2a8
meowmik <tosegida@edu.hse.ru>
2021-11-18 14:45:48 +0300

    servo.py was added (not tested)

--- lab_1/task_1/servo.py
+++ lab_1/task_1/servo.py
@@ -1,10 +1,38 @@
 from import_gpio import GPIO
-
-# BCM4
-SERVO_PIN = 7
+import time
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
+class Servo:
 
-servo_pwm = GPIO
+    # pin - 7 == BCM4
+    def __init__(self, pin = 7):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+        # todo: check attribute ("50")
+        self.pwm = GPIO.PWM(self.pin, 50)
+        # todo: check attribute ('0')
+        self.pwm.start(0)
+        self.pin = 7
+
+    def change_angle(self, angle):
+        duty = angle/18 + 2
+        GPIO.output(self.pin, True)
+        self.pwm.ChangeDutyCycle(duty)
+        # todo: check "sleep"
+        time.sleep(1)
+        GPIO.output(self.pin, False)
+        # todo: check this
+        self.pwm.ChangeDutyCycle(0)
+
+
+# todo: test it
+if __name__ == "__main__":
+    test_angles = [0, 45, 90, 180, 360]
+    servo = Servo()
+
+    for test_angle in test_angles:
+        print(f"angle - {test_angle}")
+        servo.change_angle(test_angle)
+        # todo: check "sleep"
+        time.sleep(1)
+    GPIO.cleanup()
